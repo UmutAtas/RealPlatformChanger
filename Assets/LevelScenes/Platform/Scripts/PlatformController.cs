bbdafb501786e961f21a8afca5fef349b880c142
UmutAtas <n.umutatas@gmail.com>
2022-05-09 21:10:47 +0300

    Platform movement

--- Assets/LevelScenes/Platform/Scripts/PlatformController.cs
+++ Assets/LevelScenes/Platform/Scripts/PlatformController.cs
@@ -1,15 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    [SerializeField] private List<Transform> platformList = new List<Transform>();
+    [SerializeField] private List<PlatformMover> platformList = new List<PlatformMover>();
 
+    private bool isTap;
     public enum PlatformState
     {
         right,
         mid,
         left,
     }
+
+    private void FixedUpdate()
+    {
+        MovePlatforms();
+    }
+
+    private void MovePlatforms()
+    {
+        if (!isTap)
+        {
+            foreach (var platform in platformList)
+            {
+                platform.MoveAll();
+            }
+        }
+    }
+
+    private void LockPlatformPosition()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isTap = true;
+        }
+    }
+
+    //private IEnumerator sdasd()
+    //{
+    //    foreach (var sad in platformList)
+    //    {
+    //        sad.MoveAll();
+    //        yield return new WaitForSeconds(0.2f);
+    //    }
+    //}
 }
--- Assets/LevelScenes/Platform/Scripts/PlatformMover.cs
+++ Assets/LevelScenes/Platform/Scripts/PlatformMover.cs
@@ -3,9 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using DG.Tweening;
+using MoreMountains.Tools;
 
 public class PlatformMover : MonoBehaviour
 {
+    [SerializeField] private Vector3 leftPosition;
+    [SerializeField] private Vector3 midPosition;
+    [SerializeField] private Vector3 rightPosition;
+
+    [SerializeField] private Vector3 leftScale;
+    [SerializeField] private Vector3 midScale;
+
+    [SerializeField] private float moveTime = 1f;
+    [SerializeField] private float nextMoveTime = 1f;
+    
     public PlatformController.PlatformState pState;
+
+    public void MoveLeft()
+    {
+        transform.DOLocalMove(leftPosition, moveTime);
+        transform.DOScale(leftScale, moveTime).OnComplete(() =>
+        {
+            StartCoroutine(ChangeState(PlatformController.PlatformState.left));
+        });
+    }
+
+    public void MoveRight()
+    {
+        transform.DOLocalMove(rightPosition, moveTime).OnComplete(() =>
+        {
+            StartCoroutine(ChangeState(PlatformController.PlatformState.right));
+        });
+    }
+
+    public void MoveMid()
+    {
+        transform.DOLocalMove(midPosition, moveTime);
+        transform.DOScale(midScale, moveTime).OnComplete(() =>
+        {
+            StartCoroutine(ChangeState(PlatformController.PlatformState.mid));
+        });
+    }
+
+    public void MoveAll()
+    {
+        switch (pState)
+        {
+            case PlatformController.PlatformState.right:
+                MoveMid();
+                break;
+            case PlatformController.PlatformState.mid:
+                MoveLeft();
+                break;
+            case PlatformController.PlatformState.left:
+                MoveRight();
+                break;
+        }
+    }
+
+    public IEnumerator ChangeState(PlatformController.PlatformState statePosition)
+    {
+        yield return new WaitForSeconds(nextMoveTime);
+        pState = statePosition;
+    }
     
 }
