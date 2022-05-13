71e42192138da3df137447b53b710b176be3d304
UmutAtas <n.umutatas@gmail.com>
2022-05-13 20:49:11 +0300

    PlatformPath

--- Assets/LevelScenes/Player/Scripts/PlayerMovement.cs
+++ Assets/LevelScenes/Player/Scripts/PlayerMovement.cs
@@ -5,81 +5,126 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float runTime = 5f;
-    [SerializeField] private float slowDownTime = 2f;
-    [SerializeField] private float movementSpeed = 3f;
-    [SerializeField] private float slowDownSpeed = 0.2f;
-
-    public bool canSlowDown;
-    public bool canTired;
-
-    private float startMovementSpeed;
-
-    private void Awake()
+    //[SerializeField] private float runTime = 5f;
+    //[SerializeField] private float slowDownTime = 2f;
+    //[SerializeField] private float movementSpeed = 3f;
+    //[SerializeField] private float slowDownSpeed = 0.2f;
+//
+    //public bool canSlowDown;
+    //public bool canTired;
+//
+    //private float startMovementSpeed;
+//
+    //private void Awake()
+    //{
+    //    startMovementSpeed = movementSpeed;
+    //}
+//
+    //private void Update()
+    //{
+    //    MovePlayer();
+    //}
+//
+    //private IEnumerator RunCoroutine()
+    //{
+    //    canSlowDown = false;
+    //    canTired = false;
+    //    float i = 0f;
+    //    movementSpeed = startMovementSpeed;
+    //    while (i < runTime)
+    //    {
+    //        i += Time.deltaTime;
+    //        transform.Translate(Vector3.forward * (movementSpeed * Time.deltaTime));
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //    canSlowDown = true;
+    //    StartCoroutine(SlowDownCoroutine());
+    //}
+//
+    //private IEnumerator SlowDownCoroutine()
+    //{
+    //    canTired = false;
+    //    if (canSlowDown)
+    //    {
+    //        float i = 0f;
+    //        while (i < slowDownTime)
+    //        {
+    //            i += Time.deltaTime;
+    //            if (movementSpeed > 0f)
+    //            {
+    //                movementSpeed -= slowDownSpeed;
+    //            }
+    //            transform.Translate(Vector3.forward * (movementSpeed * Time.deltaTime));
+    //            yield return new WaitForEndOfFrame();
+    //        }
+    //        canTired = true;
+    //    }
+    //    Tired();
+    //}
+//
+    //private void Tired()
+    //{
+    //    if (canTired)
+    //    {
+    //        movementSpeed = 0f;
+    //        transform.Translate(Vector3.forward * movementSpeed);
+    //    }
+    //}
+//
+    //private void MovePlayer()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        StartCoroutine(RunCoroutine());
+    //    }
+    //    else if (Input.GetMouseButtonUp(0))
+    //    {
+    //        StopAllCoroutines();
+    //    }
+    //}
+    private float Stamina,Decrease,Increase,Speed,DecreaseSpeed,BaseSpeed,DecreaseSpeedThreshold;
+    
+    void Start()
     {
-        startMovementSpeed = movementSpeed;
+        BaseSpeed = Speed;
     }
 
-    private void Update()
+    
+    void Update()
     {
-        MovePlayer();
+        GetColored();
+        if(Input.GetMouseButtonDown(0))
+            PressDown();
+        if(Input.GetMouseButtonUp(0))
+            PressUp();
     }
 
-    private IEnumerator RunCoroutine()
+    void PressDown()
     {
-        canSlowDown = false;
-        canTired = false;
-        float i = 0f;
-        movementSpeed = startMovementSpeed;
-        while (i < runTime)
+        transform.position = Vector3.Lerp(transform.position,transform.forward,Speed* Time.deltaTime);
+        Stamina -= Time.deltaTime * Decrease;
+        if (Stamina <= 75)
         {
-            i += Time.deltaTime;
-            transform.Translate(Vector3.forward * (movementSpeed * Time.deltaTime));
-            yield return new WaitForEndOfFrame();
+            Speed -= Time.deltaTime * DecreaseSpeed;
         }
-        canSlowDown = true;
-        StartCoroutine(SlowDownCoroutine());
+        else if(Stamina <= 0)
+            Lose();
     }
 
-    private IEnumerator SlowDownCoroutine()
+    void Lose()
     {
-        canTired = false;
-        if (canSlowDown)
-        {
-            float i = 0f;
-            while (i < slowDownTime)
-            {
-                i += Time.deltaTime;
-                if (movementSpeed > 0f)
-                {
-                    movementSpeed -= slowDownSpeed;
-                }
-                transform.Translate(Vector3.forward * (movementSpeed * Time.deltaTime));
-                yield return new WaitForEndOfFrame();
-            }
-            canTired = true;
-        }
-        Tired();
+        Destroy(gameObject);
     }
-
-    private void Tired()
+    void GetColored()
     {
-        if (canTired)
-        {
-            movementSpeed = 0f;
-            transform.Translate(Vector3.forward * movementSpeed);
-        }
+        float AplhaRange = Stamina;
     }
-
-    private void MovePlayer()
+    void PressUp()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            StartCoroutine(RunCoroutine());
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            StopAllCoroutines();
-        }
+        Stamina += Time.deltaTime * Increase;
+        if (Stamina >= 75)
+            Speed = BaseSpeed;
+        else if(Stamina <= 75)
+            Speed += Time.deltaTime * DecreaseSpeed;
     }
 }
