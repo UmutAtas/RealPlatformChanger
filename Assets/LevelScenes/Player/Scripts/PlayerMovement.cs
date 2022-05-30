daa846da4ad5fce6542de72acef0665a6335e238
Atakan Çakır <81085165+AztechGames@users.noreply.github.com>
2022-05-30 11:40:12 +0300

    Update PlayerMovement.cs

--- Assets/LevelScenes/Player/Scripts/PlayerMovement.cs
+++ Assets/LevelScenes/Player/Scripts/PlayerMovement.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using AmplifyShaderEditor;
 using DG.Tweening;
-using NaughtyAttributes;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
